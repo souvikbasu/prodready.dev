ae6e51ffcb6c4d3f84ab15579a02c36e559807af
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-11-25 12:44:35 +0530

    feat(homepage): add apollo-client

--- apis/apollo.client.tsx
+++ apis/apollo.client.tsx
@@ -7,17 +7,8 @@ const httpLink = createHttpLink({
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
-  // link: new RestLink({
-  //   endpoints: {
-  //     feedback: `${process.env.WALLORA_BACKEND_SUBMIT_FEEDBACK}`,
-  //     majorPlannedExpensesWorkItemwise: `${process.env.WALLORA_BACKEND_TOTAL_PLANNED_AMOUNT_WORKITEMWISE}`,
-  //     majorPlannedExpensesMonthwise: `${process.env.WALLORA_BACKEND_TOTAL_PLANNED_EXPENSES_MONTHWISE}`,
-  //   },
-  // }),
 });
 
-// NOTE: Both graphql server and mocking rest with graphql doesn't work simultaneously.
-// Setup your client.
 function GetApolloClient() {
   return client;
 }

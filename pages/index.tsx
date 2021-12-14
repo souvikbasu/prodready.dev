9da6ae1cd7f5ae0ce89177b9a02add31c45910a1
Souvik Basu <souvikbasu@gmail.com>
2021-12-13 23:21:36 -0800

    feat(analytics): add Google analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,23 @@ const Home: NextPage = () => {
             content="Learn how to create a React app that is ready for production"
           />
           <link rel="icon" href="/favicon.ico" />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
 
         <main className="min-h-screen p-4 flex flex-col justify-center items-center w-full">
